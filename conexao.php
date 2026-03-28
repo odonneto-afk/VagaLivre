84d70bcf8b8ec86275c188d7d771336f1845280e
odonneto-dev <odonneto2@ads.fiponline.edu.br>
2026-03-28 15:55:53 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,19 @@
+<?php
+$database = "u574397572_vagalivre";
+$username = "u574397572_vagalivre";
+$password = "5>YoFssoy>O";
+
+error_reporting(1);
+ob_start();
+$mysqli = new mysqli("localhost",$username,$password,$database);
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  // echo "Erro na conexao com BD: " . $mysqli -> connect_error;
+  echo "Erro na conexao com BD.";
+//   exit();
+}
+
+$empresacliente="VagaLivre";
+
+?>
